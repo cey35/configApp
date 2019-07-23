17911d4bb946dfeadcf35c4a3fabe99338b49fab
ceydakoc <ceyda.koc@ceng.deu.edu.tr>
2019-07-23 12:11:47 +0300

    angular not defined

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('myApp', ['ngRoute', 'ngStorage']);
+
+app.config(function($routeProvider, $locationProvider){
+    $locationProvider.hashPrefix('');
+    $routeProvider.when('/app', {
+        templateUrl: './pages/app1Component/app1.html',
+        controller: 'app1Controller'
+    });
+});
---
+++ public/pages/app1Component/app1Controller.js
@@ -0,0 +1,3 @@
+app.controller('app1Controller', function($scope, $http){
+
+})

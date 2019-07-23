ea7f4f1ce5b975e301d20fcb0b84e920d542b973
ceydakoc <ceyda.koc@ceng.deu.edu.tr>
2019-07-23 15:11:13 +0300

    frontend - no error

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ['ngRoute', 'ngStorage']);
 
 app.config(function($routeProvider, $locationProvider){
     $locationProvider.hashPrefix('');
-    $routeProvider.when('app', {
+    $routeProvider.when('/app', {
         templateUrl: './pages/app1Component/app1.html',
         controller: 'app1Controller'
     });
--- public/pages/app1Component/app1Controller.js
+++ public/pages/app1Component/app1Controller.js
@@ -1,3 +1,3 @@
 app.controller('app1Controller', function($scope, $http){
-    console.log("deneme");
+    
 })
